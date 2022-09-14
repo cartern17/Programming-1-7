3a479129c9fac34f2bf20be2bb3629f8505a440b
cartern17 <112727223+cartern17@users.noreply.github.com>
2022-09-14 19:33:10 +0000

    updt

---
+++ prog54c.py
@@ -0,0 +1,7 @@
+radius = float(input("Please enter radius "))
+
+area = round(radius * radius * 3.14159, 3)
+circumference  = round(2 * 3.14159 * radius, 3)
+
+print("\nThe area is", area)
+print("\nThe circumference is", circumference)
