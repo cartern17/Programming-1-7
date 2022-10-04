9426616a03ac95eb186f4e5f57fb34abd80cbd86
cartern17 <112727223+cartern17@users.noreply.github.com>
2022-10-04 18:21:43 +0000

    update

---
+++ k46.py
@@ -0,0 +1,219 @@
+from stanfordkarel import *
+from time import sleep
+
+class ktools: 
+  def m(self):
+    """Shorthand for Move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+    self.tl()
+    self.tl()
+    self.tl()
+
+
+  def ta(self):
+    """Turn Around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+
+
+  def put2(self):
+    """Put 2 beepers in a line"""
+    self.put()
+    self.m()
+    self.put()
+
+  def put5(self):
+    """Put 5 beepers in a line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  def h(self):
+    """Print H usin beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+
+  def fic(self) -> bool:
+    """Front Is Clear"""
+    return front_is_clear()
+
+  def fib(self) -> bool:
+    """Front Is Blocked"""
+    return not self.fic()
+
+  def ric(self) -> bool:
+    """Right Is Clear"""
+    self.tr()
+    if self.fic():
+      self.tl()
+      return True
+    self.tl()
+    return False
+
+  def rib(self) -> bool:
+    """Right Is Blocked"""
+    return not self.ric()
+
+  def lic(self) -> bool:
+    self.tl()
+    if self.fic():
+      self.tr()
+      return True
+    self.tr()
+    return False
+
+  def lib(self) -> bool:
+    return not self.ric()
+
+  def mazemove(self):
+    """Maze Move"""
+    if self.fib():
+      self.tl()
+    else:
+      self.m()
+      if self.ric():
+        self.tr()
+        self.m()
+        if self.ric():
+          self.tr()
+          self.m()
+
+
+  def mm(self, num):
+    for number in range(0, num):
+      self.m()
+
+  def putm(self, num):
+    for i in  range(num - 1):
+      self.put()
+      self.m()
+    self.put()
+
+  def pickm(self, num):
+    for _ in range(num - 1):
+      self.pick()
+      self.m()
+    self.pick()
+
+  def SOB(self) -> bool:
+    """Standing On Beeper"""
+    return beepers_present()
+
+  def jump(self):
+    """Jump for 510"""
+    while self.fic():
+      self.m()
+    self.tl()
+    while self.rib():
+      self.m()
+    self.tr()
+    self.m()
+    self.tr()
+    while self.fic():
+      self.m()
+    self.tl()
+
+  def find(self):
+    """Find for 515"""
+    while not facing_north():
+      self.tl()
+    self.m()
+    if not self.SOB():
+      self.tl()
+      self.m()
+      self.tl()
+      self.m()
+    for _ in range(2):
+      if not self.SOB():
+        self.m()
+        self.tl()
+        self.m()
+
+  def hurdles(self):
+    self.tl()
+    self.mm(2)
+    self.tr()
+    self.m()
+    self.tr()
+    self.mm(2)
+    self.ta()
+    self.m()
+    self.tr()
+    self.m()
+    self.tr()
+    self.m()
+    self.tl()
+    self.m()
+    self.tl()
+    self.mm(3)
+    self.tr()
+    self.m()
+    self.tr()
+    self.mm(3)
+    self.tl()
+    self.m()
+    self.tl()
+    self.mm(2)
+    self.tr()
+    self.m()
+    self.tr()
+    self.mm(2)
+    self.tl()
+    self.m()
+    self.tl()
+    self.mm(2)
+    self.tr()
+    self.m()
+    self.tr()
+    self.mm(2)
+    self.tl()
+
+    pass
+
+    
+
+def main():
+    """ Karel code goes here! """
+    kt = ktools()
+    kt.hurdles()
+    pass
+
+
+
+if __name__ == "__main__":
+    run_karel_program()
