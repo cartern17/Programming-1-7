fdb487846379bfdaf9f49d85d8a7d7421db92957
cartern17 <112727223+cartern17@users.noreply.github.com>
2022-12-02 20:35:49 +0000

    3

--- LP4-3.py
+++ LP4-3.py
@@ -2,23 +2,22 @@ def main():
   eggs = int(input("Enter the total number of eggs: "))
   dozens = eggs // 12
   remainder = eggs % 12
-  price = 0.0
   cost = 0.0
 
   if dozens <= 3:
-    cost == 0.50
+    cost = 0.50
   elif dozens <= 5 and dozens >= 4:
-    cost == 0.45
+    cost = 0.45
   elif dozens <= 10 and dozens >= 6:
-    cost == 0.40
+    cost = 0.40
   elif dozens >= 11:
-    cost == 0.35
-  
-    
-  
-  
+    cost = 0.35
 
-  pass
+
+  total = cost * dozens + remainder * (cost / 12.0)
+
+  print("The bill is equal to: $", round(total, 2)
+
 
 
 if __name__ == "__main__":
