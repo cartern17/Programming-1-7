13205a77a849194af54e485a5ac299f34f8b3e2e
LillyA2 <112727168+LillyA2@users.noreply.github.com>
2023-01-10 20:36:15 +0000

    update

--- breakout.py
+++ breakout.py
@@ -23,33 +23,45 @@ coloums = 8
 
 class brick():
   def __init__(self):
-    self.width = screen_width // coloumns
+    self.width = screen_width // coloums
     self.height = 40
 
   def create_brick(self):
-    self.brick = {}
-    brick_singular = {}
+    self.brick = []
+    brick_singular = []
     for row in range(rows):
-      brick_row = {}
+      brick_row = []
       for cols in range(coloums):
         brick_x = coloums * self.width
         brick_y = rows * self.height
-        rect = pygame.Rect(brick_x, brick_y, self.width, self.height)
+        rect = pygame.rect(brick_x, brick_y, self.width, self.height)
         if rows <= 2:
           strength = 3
         elif rows <= 4:
           strength = 2
         elif rows <= 6:
           strength = 1
-        brick_singular = {rect, strength}
+        brick_singular = [rect, strength]
         brick_row.append(brick_singular)
       self.brick.append(brick_row)
       
   def show_brick(self):
     for row in self.brick:
       for brick in row:
-        if brick{1}
-   
+        if brick[1] == 3:
+          brick_col = bl
+        elif brick[1] == 2: 
+          brick_col = yl
+        elif brick[1] == 1:
+          brick_col = rd
+        pygame.draw.rect(screen, brick_col, brick[1])
+        pygame.draw.rect(screen.blit, brick[1])
+
+
+brick = brick()
+brick.create_brick()
+pass 
+    
 run = True
 while run:
     for event in pygame.event.get():
